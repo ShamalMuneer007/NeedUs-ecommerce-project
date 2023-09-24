e59c5c5ae9c25a95ca1e37d6c019c14c7290fd6a
Shamal Muneer <shamal20017@gmail.com>
2023-09-25 01:03:03 +0530

    "Added some user and product functionalities"

---
+++ src/main/java/com/needus/ecommerce/entity/product/ProductFilters.java
@@ -0,0 +1,24 @@
+package com.needus.ecommerce.entity.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductFilters {
+    @Id
+    @SequenceGenerator(name = "filterSequence",allocationSize = 1)
+    @GeneratedValue(generator = "filterSequence",strategy = GenerationType.SEQUENCE)
+    @Column(name = "filterId")
+    private Integer filterId;
+    @Column(name = "filterName")
+    private String filterName;
+    @ManyToOne
+    @JoinColumn(name="category_id",unique = false)
+    private Categories category;
+
+}
